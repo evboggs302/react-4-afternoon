5927537f02051c0678e56cc96fb6c8c998f1d87a
Evan Boggs Jr <evanboggs@Evans-MacBook-Pro.local>
2019-05-21 15:21:30 -0700

    step7 complete

--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,14 +1,33 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class Student extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      studentInfo: {}
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(`http://localhost:3005/students/${this.props.match.params.id}`)
+      .then(response => {
+        this.setState({
+          studentInfo: response.data
+        });
+      });
   }
 
   render() {
+    console.log(this.state.studentInfo);
+    let { email, first_name, last_name, grade, id } = this.state.studentInfo;
     return (
-      <div className="box">
+      <div className="box" key={id}>
         <h1>Student</h1>
+        <h1>{`${last_name}, ${first_name}`}</h1>
+        <h3>{`Grade: ${grade}`}</h3>
+        <h3>{`Email: ${email}`}</h3>
       </div>
     );
   }
